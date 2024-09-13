0186bee3c48007552117e247d9873440f6634893
yuc <yuc@lawsdata.com>
2024-09-13 16:01:57 +0800

    feat: init proj

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './assets/main.css'
+import App from './App.vue'
+
+export const testSparkyApi = function() {
+    console.log('test sparky api success!')
+}
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    lib: {
+      entry: './src/index.ts', // 入口文件
+      name: 'sparkyie/api',
+      fileName: 'sparkyie-api',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  }
+})
